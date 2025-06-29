c39a3a75e49586bd9119244b3204b60581e97983
HYEHUE <samuelmata0909@gmail.com>
2025-06-29 16:57:50 -0400

    Refactor code structure for improved readability and maintainability

--- kubejs/server_scripts/oregen.js
+++ kubejs/server_scripts/oregen.js
@@ -1,5 +1,8 @@
 ServerEvents.lowPriorityData(event => {
 
+    removeFeature(event, "minecraft:ore_copper")
+
+    addOregenOverworld(event, "kubejs:ore_copper", "minecraft:copper_ore", "minecraft:trapezoid", -40, 60, 3, 5, 0)
     addOregenOverworld(event, "kubejs:ore_aluminium", "gtceu:aluminium_ore", "minecraft:trapezoid", -40, 16, 3, 5, 0)
 
 })
