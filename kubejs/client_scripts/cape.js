4d09c55a61eaa011f3f4b3c5a0b111568098dd5b
HYEHUE <samuelmata0909@gmail.com>
2025-06-29 15:19:20 -0400

    New configs and Mods

--- kubejs/client_scripts/cape.js
+++ kubejs/client_scripts/cape.js
@@ -1,3 +1,4 @@
 CapeJS.addCapes(event => {
     event.register("175edcc3-ab9b-3439-a488-e1aa32a28d9e", "creeper_gold_smp");
+    event.register("0fddba54-4cd7-4be9-ac06-e3b5977b245e", "twitch_cape");
 });
