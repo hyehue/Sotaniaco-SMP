57c9bc155782aa3475e68be4d06a9b8a6eba8554
HYEHUE <samuelmata0909@gmail.com>
2025-06-29 18:53:57 -0400

    Add additional cape registrations for improved customization

--- kubejs/client_scripts/cape.js
+++ kubejs/client_scripts/cape.js
@@ -1,4 +1,8 @@
 CapeJS.addCapes(event => {
     event.register("175edcc3-ab9b-3439-a488-e1aa32a28d9e", "creeper_gold_smp");
+    event.register("f85eb091-c0db-3f59-9efb-21394dadbe01", "creeper_gold_smp");
+    event.register("cdc5dab9-748c-326b-9f0e-591c371a2e36", "creeper_gold_smp");
+    event.register("52ec7480-9757-39e2-b698-65629d820c9b", "creeper_gold_smp");
+    event.register("df9345a0-5f55-36b7-bef3-d920b2ef860f", "creeper_gold_smp");
     event.register("0fddba54-4cd7-4be9-ac06-e3b5977b245e", "twitch_cape");
 });
